2e796ca785f15da3544014017e454c8aca31bfa6
redouane <redouaneelrhachi@gmail.com>
2020-12-21 10:42:54 +0100

    fix bsod

--- main/bsod.cpp
+++ main/bsod.cpp
@@ -23,7 +23,7 @@
 
 static const char *crash_emailaddr =
 #ifndef CRASH_EMAILADDR
-	"DreamArabia Community";
+	"the BlackHole forum";
 #else
 	CRASH_EMAILADDR;
 #endif
@@ -88,15 +88,25 @@ static void getKlog(FILE* f)
 	fprintf(f, "%s\n", &buf[0]);
 }
 
-static void stringFromFile(FILE* f, const char* context, const char* filename)
+static const std::string stringFromFile(const char* filename)
 {
+	std::string retval = "";
+	std::string newline = "";
 	std::ifstream in(filename);
 
 	if (in.good()) {
-		std::string line;
-		std::getline(in, line);
-		fprintf(f, "%s=%s\n", context, line.c_str());
+		do {
+			std::string line;
+			std::getline(in, line);
+			if(line.length() > 0) {
+				retval += newline;
+				newline = '\n';
+				retval += line.c_str();
+			}
+		} while (in.good());
+		in.close();
 	}
+	return retval;
 }
 
 static bool bsodhandled = false;
@@ -140,14 +150,8 @@ void bsodFatal(const char *component)
 	bsodrestart = true;
 
 	/* show no more than one bsod while shutting down/crashing */
-	if (bsodhandled) {
-		if (component) {
-			eSyncLog();
-			sleep(1);
-			raise(SIGKILL);
-		}
+	if (bsodhandled)
 		return;
-	}
 	bsodhandled = true;
 
 	if (!component)
@@ -163,10 +167,14 @@ void bsodFatal(const char *component)
 	FILE *f;
 	std::string crashlog_name;
 	std::ostringstream os;
-	std::ostringstream os_text;
+	time_t t = time(0);
+	struct tm tm;
+	char tm_str[32];
+	localtime_r(&t, &tm);
+	strftime(tm_str, sizeof(tm_str), "%Y-%m-%d_%H-%M-%S", &tm);
 	os << getConfigString("config.crash.debug_path", "/home/root/logs/");
-	os << "enigma2_crash_";
-	os << time(0);
+	os << "Enigma2_crash_";
+	os << tm_str;
 	os << ".log";
 	crashlog_name = os.str();
 	f = fopen(crashlog_name.c_str(), "wb");
@@ -177,14 +185,14 @@ void bsodFatal(const char *component)
 		 * alone because we may be in a crash loop and writing this file
 		 * all night long may damage the flash. Also, usually the first
 		 * crash log is the most interesting one. */
-		crashlog_name = "/home/root/logs/enigma2_crash.log";
+		crashlog_name = "/home/root/logs/Enigma2_crash.log";
 		if ((access(crashlog_name.c_str(), F_OK) == 0) ||
 		    ((f = fopen(crashlog_name.c_str(), "wb")) == NULL))
 		{
 			/* Re-write the same file in /tmp/ because it's expected to
 			 * be in RAM. So the first crash log will end up in /home
 			 * and the last in /tmp */
-			crashlog_name = "/tmp/enigma2_crash.log";
+			crashlog_name = "/tmp/Enigma2_crash.log";
 			f = fopen(crashlog_name.c_str(), "wb");
 		}
 	}
@@ -199,31 +207,22 @@ void bsodFatal(const char *component)
 		strftime(tm_str, sizeof(tm_str), "%a %b %_d %T %Y", &tm);
 
 		fprintf(f,
-			"OpenBlackHole Enigma2 crash log\n\n"
-			"crashdate=%s\n"
-			"compiledate=%s\n"
-			"skin=%s\n"
-			"sourcedate=%s\n"
-			"branch=%s\n"
-			"rev=%s\n"
-			"component=%s\n\n",
-			tm_str,
-			__DATE__,
-			getConfigString("config.skin.primary_skin", "Default Skin").c_str(),
-			enigma2_date,
-			enigma2_branch,
-			enigma2_rev,
-			component);
-
-		stringFromFile(f, "stbmodel", "/proc/stb/info/boxtype");
-		stringFromFile(f, "stbmodel", "/proc/stb/info/vumodel");
-		stringFromFile(f, "stbmodel", "/proc/stb/info/model");
-		stringFromFile(f, "stbmodel", "/proc/stb/info/hwmodel");
-		stringFromFile(f, "stbmodel", "/proc/stb/info/gbmodel");
-		stringFromFile(f, "kernelcmdline", "/proc/cmdline");
-		stringFromFile(f, "nimsockets", "/proc/bus/nim_sockets");
-		stringFromFile(f, "imageversion", "/etc/image-version");
-		stringFromFile(f, "imageissue", "/etc/issue.net");
+					"OpenBh Enigma2 Crashlog\n\n"
+					"Crashdate = %s\n\n"
+					"%s\n"
+					"Compiled = %s\n"
+					"Skin = %s\n"
+					"Component = %s\n\n"
+					"Kernel CMDline = %s\n"
+					"Nim Sockets = %s\n",
+					tm_str,
+					stringFromFile("/etc/image-version").c_str(),
+					__DATE__,
+					getConfigString("config.skin.primary_skin", "Default Skin").c_str(),
+					component,
+					stringFromFile("/proc/cmdline").c_str(),
+					stringFromFile("/proc/bus/nim_sockets").c_str()
+				);
 
 		/* dump the log ringbuffer */
 		fprintf(f, "\n\n");
@@ -235,7 +234,6 @@ void bsodFatal(const char *component)
 		/* dump the kernel log */
 		getKlog(f);
 
-		fsync(fileno(f));
 		fclose(f);
 	}
 
@@ -254,7 +252,7 @@ void bsodFatal(const char *component)
 	gPainter p(my_dc);
 	p.resetOffset();
 	p.resetClip(eRect(ePoint(0, 0), my_dc->size()));
-	p.setBackgroundColor(gRGB(0x27408B));
+	p.setBackgroundColor(gRGB(0x010000));
 	p.setForegroundColor(gRGB(0xFFFFFF));
 	p.clear();
 
@@ -266,8 +264,7 @@ void bsodFatal(const char *component)
 
 	os.str("");
 	os.clear();
-	os_text.clear();
-
+  os_text.clear();
 
 	if (!bsodpython)
 	{
@@ -348,7 +345,6 @@ void bsodFatal(const char *component)
 		usable_area = eRect(hd ? 30 : 100, hd ? 180 : 170, my_dc->size().width() - (hd ? 60 : 180), my_dc->size().height() - (hd ? 30 : 20));
 		p.renderText(usable_area, logtail, gPainter::RT_HALIGN_LEFT);
 	}
-	eSyncLog();
 	sleep(10);
 
 	/*
@@ -362,25 +358,26 @@ void bsodFatal(const char *component)
 	 * executing here.
 	 */
 
-	if (bsodpython)
-	{
-		bsodrestart = false;
-		bsodhandled = false;
-		p.setBackgroundColor(gRGB(0,0,0,0xFF));
-		p.clear();
-		return;
-	}
+	 if (bsodpython)
+ {
+	 bsodrestart = false;
+	 bsodhandled = false;
+	 p.setBackgroundColor(gRGB(0,0,0,0xFF));
+	 p.clear();
+	 return;
+ }
+ 
 	if (component) raise(SIGKILL);
 }
 
 #if defined(__MIPSEL__)
 void oops(const mcontext_t &context)
 {
-	eDebug("PC: %08lx", (unsigned long)context.pc);
+	eLog(lvlFatal, "PC: %08lx", (unsigned long)context.pc);
 	int i;
 	for (i=0; i<32; i += 4)
 	{
-		eDebug("%08x %08x %08x %08x",
+		eLog(lvlFatal, "    %08x %08x %08x %08x",
 			(int)context.gregs[i+0], (int)context.gregs[i+1],
 			(int)context.gregs[i+2], (int)context.gregs[i+3]);
 	}
@@ -396,21 +393,22 @@ void print_backtrace()
 {
 	void *array[15];
 	size_t size;
-	int cnt;
+	size_t cnt;
 
 	size = backtrace(array, 15);
-	eDebug("Backtrace:");
-	for (cnt = 1; static_cast<unsigned>(cnt) < size; ++cnt)
+	eLog(lvlFatal, "Backtrace:");
+	for (cnt = 1; cnt < size; ++cnt)
 	{
 		Dl_info info;
 
 		if (dladdr(array[cnt], &info)
 			&& info.dli_fname != NULL && info.dli_fname[0] != '\0')
 		{
-			eDebug("%s(%s) [0x%X]", info.dli_fname, info.dli_sname != NULL ? info.dli_sname : "n/a", (unsigned long int) array[cnt]);
+			eLog(lvlFatal, "%s(%s) [0x%X]", info.dli_fname, info.dli_sname != NULL ? info.dli_sname : "n/a", (unsigned long int) array[cnt]);
 		}
 	}
 }
+
 
 void handleFatalSignal(int signum, siginfo_t *si, void *ctx)
 {
@@ -419,7 +417,7 @@ void handleFatalSignal(int signum, siginfo_t *si, void *ctx)
 	oops(uc->uc_mcontext);
 #endif
 	print_backtrace();
-	eDebug("-------FATAL SIGNAL");
+	eLog(lvlFatal, "-------FATAL SIGNAL (%d)", signum);
 	bsodFatal("enigma2, signal");
 }
 
